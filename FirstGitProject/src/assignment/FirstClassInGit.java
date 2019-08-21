b2823abe616e916f4d267a583ec4123111ecee86
zs00508 <zs00508@CEIT2553214X008.ad.georgiasouthern.edu>
2019-08-21 16:42:14 -0400

    Adding Scanner to Accept last name

--- FirstGitProject/src/assignment/FirstClassInGit.java
+++ FirstGitProject/src/assignment/FirstClassInGit.java
@@ -1,10 +1,14 @@
 package assignment;
-
+import java.util.Scanner;
 public class FirstClassInGit {
 
 	public static void main(String[] args) {
 		System.out.println("Hello my name is Zachary!");
-		
+		Scanner input = new Scanner(System.in);
+		System.out.println("What is your last name?");
+		String g = input.next();
+		System.out.println("Your last name is " + g);
+		input.close();
 		//add a statement to display your name in the console
 		
 		//Then commit your changes
